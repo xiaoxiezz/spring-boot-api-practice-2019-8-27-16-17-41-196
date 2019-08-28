030bf2eded043c65bc0d3078c1632ef64b39ce2f
xiexinfang <1036554353@qq.com>
2019-08-28 21:35:54 +0800

    company employee class

--- src/main/java/com/tw/apistackbase/ApiStackBaseApplication.java
+++ src/main/java/com/tw/apistackbase/ApiStackBaseApplication.java
@@ -2,9 +2,19 @@ package com.tw.apistackbase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
 
 @SpringBootApplication
 public class ApiStackBaseApplication {
+
+	@GetMapping(path = "/companies")
+	public HashMap<String,String> getCompanies(){
+		HashMap<String,String> map = new HashMap<>();
+		map.put("user","name");
+		return  map;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiStackBaseApplication.class, args);
---
+++ src/main/java/com/tw/apistackbase/controller/Company.java
@@ -0,0 +1,13 @@
+package com.tw.apistackbase.controller;
+
+public class Company {
+    private String companyName;
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+}
