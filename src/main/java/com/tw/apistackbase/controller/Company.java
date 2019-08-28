7d9ad82c9f5fedc21be84bfb27248a57ba16e52c
xiexinfang <1036554353@qq.com>
2019-08-28 21:49:41 +0800

    company employee class

--- src/main/java/com/tw/apistackbase/controller/Company.java
+++ src/main/java/com/tw/apistackbase/controller/Company.java
@@ -1,13 +1,46 @@
 package com.tw.apistackbase.controller;
 
-public class Company {
-    private String companyName;
+import java.util.List;
 
+public class Company {
+    private int id;
+    private String companyName;
+    private int employeesNumber;
+    private List<Employee> employees;
+
+    public Company() {
+        super();
+    }
+    public Company(int id, String companyName, int employeesNumber, List<Employee> employees) {
+        super();
+        this.id = id;
+        this.companyName = companyName;
+        this.employeesNumber = employeesNumber;
+        this.employees = employees;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
     public String getCompanyName() {
         return companyName;
     }
-
     public void setCompanyName(String companyName) {
         this.companyName = companyName;
     }
+    public int getEmployeesNumber() {
+        return employeesNumber;
+    }
+    public void setEmployeesNumber(int employeesNumber) {
+        this.employeesNumber = employeesNumber;
+    }
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
 }
+
